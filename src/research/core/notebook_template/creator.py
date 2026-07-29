814aae1cd70b5a79ef1040890451dea96e6c127d
Weorano <ignatov.roman.a@mail.ru>
2026-07-29 16:26:51 +0300

    framework #6 added core creator

---
+++ src/research/core/notebook_template/creator.py
@@ -0,0 +1,78 @@
+import json
+from pathlib import Path
+from typing import Dict, Any, List
+
+import pandas as pd
+
+from .context import NotebookContext
+from .notebook_spec import NotebookSpec
+from .structural_elements.container import ContainerBuilder
+from .structural_elements.python_code import VisualizationCodeStructurer
+
+
+class NotebookCreator:
+    def __init__(self, config: Dict[str, Any]):
+        self.config = config
+
+    def create(
+            self,
+            spec: NotebookSpec,
+            notebook_path: Path,
+            loaded_dataframes: None | Dict[str, pd.DataFrame] = None,
+            first_create: bool = True
+    ) -> None:
+        if first_create:
+            self._create_empty_notebook(notebook_path)
+
+        if loaded_dataframes is None:
+            loaded_dataframes = self._load_dfs()
+
+        ctx = self._generate_notebook_structure(loaded_dataframes, spec)
+
+        self._append_in_notebook_body(notebook_path, ctx)
+
+    @staticmethod
+    def _create_empty_notebook(notebook_path: Path) -> None:
+        with open(notebook_path, "w", encoding="utf-8") as f:
+            json.dump(
+                {"cells": [], "metadata": {}, "nbformat": 4, "nbformat_minor": 5},
+                f,
+            )
+
+    def _load_dfs(self) -> Dict[str, pd.DataFrame]:
+        return {
+            name: pd.read_csv(path)
+            for name, path in self.config["datasets_paths"].items()
+        }
+
+    def _generate_notebook_structure(
+            self,
+            loaded_dataframes: Dict[str, pd.DataFrame],
+            spec: NotebookSpec,
+    ) -> List[Dict[str, Any]]:
+        ctx = NotebookContext(
+            loaded_dataframes = loaded_dataframes,
+            config = self.config,
+            container_builder = ContainerBuilder(),
+            visualization_code_structurer=VisualizationCodeStructurer(),
+        )
+
+        for section in spec.sections:
+            section(ctx)
+
+        return ctx.cells
+
+    @staticmethod
+    def _append_in_notebook_body(
+            notebook_path: Path,
+            cells: List[Dict[str, Any]],
+    ) -> None:
+        with open(notebook_path, "r", encoding="utf-8") as f:
+            nb = json.load(f)
+
+        nb["cells"].extend(cells)
+
+        with open(notebook_path, "w", encoding="utf-8") as f:
+            json.dump(nb, f, indent=2, ensure_ascii=False)
+
+
