6a889e416bbff180d648622341363c37fe4a86ed
Weorano <ignatov.roman.a@mail.ru>
2026-09-04 12:23:20 +0300

    framework #11 added work area for pipeline-compatible actions
    Here, all the data preprocessing transformers are gradually formed, and they must be activated before the model is trained.

---
+++ research/preprocessing/__init__.py
@@ -0,0 +1,7 @@
+from research.preprocessing.surface_processing.transformer import SurfaceProcessingTransformer
+from research.preprocessing.advanced_processing.transformer import AdvancedProcessingTransformer
+
+__all__ = [
+    "SurfaceProcessingTransformer",
+    "AdvancedProcessingTransformer",
+]
